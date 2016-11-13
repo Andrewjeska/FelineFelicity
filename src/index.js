4354359d1d70a94ca76ff4de84fc6a7f18fc6673
Michael <m.anderjaska@gmail.com>
2016-11-12 22:21:37 -0500

    remove router

--- src/index.js
+++ src/index.js
@@ -3,17 +3,20 @@
 //import 'core-js/fn/object/assign';
 import React from 'react';
 //import ReactDOM from 'react-dom';
-const ReactDOM = require('react-dom');
+const render = require('react-dom').render;
 import Landing from './components/Landing';
 import Search from './components/SearchResults';
 import { Router, Route, Link, browserHistory } from 'react-router'
 
 // Render the main component into the dom
-/*
-<Router history={browserHistory}>
-<Route path="/" component={Landing}/>
-    <Route path="search" component={Search}/>
-</Router>
-*/
 
-ReactDOM.render(<Landing></Landing>, document.getElementById('root'));
+
+
+
+render((
+    <Router history={browserHistory}>
+    <Route path="/" component={Landing}/>
+        <Route path="search" component={Search}/>
+    </Router>
+
+), document.getElementById('root'));
