1697a269d0880fd7a60295b9b669001c54a362e0
Michael <m.anderjaska@gmail.com>
2016-12-01 17:45:26 -0500

    get working

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ if(process.env.NODE_ENV !== 'production') {
   app.use(webpackHotMiddleware(compiler));
 }
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 
 
 app.get('*', function(request, response) {
-  response.sendFile(__dirname + '/dist/index.html')
+  response.sendFile(path.join(__dirname + '/index.html'))
 });
 
 app.use('/api', api );
