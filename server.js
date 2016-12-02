f50fc1eb3338beb53335708cb26d46434af2c789
Michael <m.anderjaska@gmail.com>
2016-12-02 11:07:09 -0500

    fixed url error

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ if(process.env.NODE_ENV !== 'production') {
 
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
-
+//path.resolve(__dirname, 'index.html')
 app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, 'index.html'))
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 });
 
 app.use('/api', api );
